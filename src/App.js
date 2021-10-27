28b0314c0c364c1fcbd172398ed56d516eea32cc
Real Ivan Nosov <i-am-ivan-nosov@yandex.com>
2021-10-28 00:55:32 +0300

    add clarifai face recognation api

--- src/App.js
+++ src/App.js
@@ -1,35 +1,146 @@
 import React from 'react';
 import './App.css';
+import Clarifai from 'clarifai';
 import Navigation from './components/navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import 'tachyons';
 import Particles from "react-tsparticles";
 
-function App() {
-  const particlesInit = (main) => {
-    console.log(main);
+const app = new Clarifai.App({
+  apiKey: '8f616fe66f584932a86da2784d8d1613'
+});
 
-    // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
-  };
+const particlesOptions = {
+  background: {
+    color: {
+      value: "transparent",
+    },
+  },
+  fpsLimit: 60,
+  interactivity: {
+    detectsOn: "canvas",
+    events: {
+      onClick: {
+        enable: true,
+        mode: "push",
+      },
+      onHover: {
+        enable: true,
+        mode: "repulse",
+      },
+      resize: true,
+    },
+    modes: {
+      bubble: {
+        distance: 400,
+        duration: 2,
+        opacity: 0.8,
+        size: 40,
+      },
+      push: {
+        quantity: 4,
+      },
+      repulse: {
+        distance: 200,
+        duration: 0.4,
+      },
+    },
+  },
+  particles: {
+    color: {
+      value: "#ffffff",
+    },
+    links: {
+      color: "#ffffff",
+      distance: 150,
+      enable: true,
+      opacity: 0.5,
+      width: 1,
+    },
+    collisions: {
+      enable: true,
+    },
+    move: {
+      direction: "none",
+      enable: true,
+      outMode: "bounce",
+      random: false,
+      speed: 2,
+      straight: false,
+    },
+    number: {
+      density: {
+        enable: true,
+        value_area: 800,
+      },
+      value: 80,
+    },
+    opacity: {
+      value: 0.5,
+    },
+    shape: {
+      type: "circle",
+    },
+    size: {
+      random: true,
+      value: 5,
+    },
+  },
+  detectRetina: true,
+}
 
-  const particlesLoaded = (container) => {
-    console.log(container);
-  };
-  return (
-    <div className="App">
-      <Particles id="tsparticles" url="http://foo.bar/particles.json" init={particlesInit} loaded={particlesLoaded} />
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imgUrl: '',
+    }
+  }
 
-      {/* 
-      
-      <FaceRecognition /> */}
-    </div>
-  );
+  onClick = () => {
+    this.setState({imgUrl: this.state.input});
+    app.models.predict(
+      Clarifai.FACE_DETECT_MODEL,
+      this.state.input) 
+      .then(
+        function (response) {
+          console.log(response.outputs[0].data.regions[0].region_info.bounding_box)
+        },
+        function (err) {
+
+        }
+      );
+  }
+
+  onInputChange = (evt) => {
+    this.setState({input: evt.target.value})
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Particles
+          className='particles'
+          width='100vw'
+          height='100vh'
+          id="tsparticles"
+          options={particlesOptions}
+        />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onClick={this.onClick} />
+        <FaceRecognition imgUrl={this.state.imgUrl}/>
+      </div>
+    );
+  }
+
 }
 
 export default App;
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './ImageLinkForm.css';
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({onInputChange, onClick}) => {
     return (
         <div>
             <p className='f5'>
@@ -9,8 +9,8 @@ const ImageLinkForm = () => {
             </p>
             <div style={{ display: 'flex', justifyContent: 'center' }}>
                 <div className='form center pa4 br3 shadow-5'>
-                    <input className='f4 pa2 w-70 center block' type='text' />
-                    <button className='center w-30 grow f4 link ph3 pv2 dib white bg-light-purple'>Найти</button>
+                    <input className='f4 pa2 w-70 center block' type='text' onInput={onInputChange}/>
+                    <button className='center w-30 grow f4 link ph3 pv2 dib white bg-light-purple' onClick={onClick}>Найти</button>
                 </div>
             </div>
         </div>
